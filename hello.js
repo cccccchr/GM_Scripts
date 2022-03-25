761667fab729bfb464675ab3a37c6e9cde35dbc8
Chr_ <chr@chrxw.com>
2022-03-25 20:58:58 +0800

    first script

---
+++ hello.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://github.com/cccccchr/GM_Scripts
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    alert("hello world");
+})();
